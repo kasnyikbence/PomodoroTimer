fd3c9794b4224e2b874e6c6a36c5b198cd6b4ed4
Kasnyik Bence <h263908@stud.u-szeged.hu>
2026-03-17 11:44:55 +0100

    generate all components

---
+++ pomodoro_timer/src/app/components/timer-component/timer-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timer-component',
+  imports: [],
+  templateUrl: './timer-component.html',
+  styleUrl: './timer-component.scss',
+})
+export class TimerComponent {}
