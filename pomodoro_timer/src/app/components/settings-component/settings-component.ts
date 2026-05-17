5c53e684f30d84a01fd40b22bd6cc18f64b9d91d
Kasnyik Bence <h263908@stud.u-szeged.hu>
2026-05-17 10:47:34 +0200

    bug fixes

--- pomodoro_timer/src/app/components/settings-component/settings-component.ts
+++ pomodoro_timer/src/app/components/settings-component/settings-component.ts
@@ -1,4 +1,5 @@
-import { Component, inject, OnInit } from "@angular/core";
+import { AsyncPipe } from "@angular/common";
+import { Component, inject } from "@angular/core";
 import { SettingsService } from "../../services/settings-service";
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
 import { MatInputModule } from "@angular/material/input";
@@ -7,44 +8,97 @@ import { TimerService } from "../../services/timer-service";
 import { NavbarComponent } from "../navbar-component/navbar-component";
 import { MatIcon } from "@angular/material/icon";
 import { BackgroundComponent } from "../background-component/background-component";
+import { Observable, Subject, catchError, debounceTime, distinctUntilChanged, filter, map, of, startWith, switchMap, tap, withLatestFrom } from "rxjs";
 
 
 @Component({
   selector: "app-settings-component",
   standalone: true,
-  imports: [ReactiveFormsModule, MatInputModule, NavbarComponent, MatIcon, BackgroundComponent],
+  imports: [ReactiveFormsModule, MatInputModule, NavbarComponent, MatIcon, BackgroundComponent, AsyncPipe],
   templateUrl: "./settings-component.html",
   styleUrl: "./settings-component.scss",
 })
-export class SettingsComponent implements OnInit {
+export class SettingsComponent {
   constructor(private timerService: TimerService) {}
   private router = inject(Router);
   public settingsService = inject(SettingsService);
 
+  private readonly saveRequested$ = new Subject<void>();
+
   settingsForm = new FormGroup({
-    focusTime: new FormControl(this.settingsService.focusTime()),
-    shortBreakTime: new FormControl(this.settingsService.shortBreakTime(), Validators.min(1)),
-    longBreakTime: new FormControl(this.settingsService.longBreakTime(), Validators.min(1)),
+    focusTime: new FormControl(this.settingsService.focusTime(), {
+      nonNullable: true,
+      validators: [Validators.required, Validators.min(1)],
+    }),
+    shortBreakTime: new FormControl(this.settingsService.shortBreakTime(), {
+      nonNullable: true,
+      validators: [Validators.required, Validators.min(1)],
+    }),
+    longBreakTime: new FormControl(this.settingsService.longBreakTime(), {
+      nonNullable: true,
+      validators: [Validators.required, Validators.min(1)],
+    }),
   });
 
-  ngOnInit() {
-    this.settingsForm.valueChanges.subscribe((changes) => {
-      console.log("Valaki épp módosítja az értékeket:", changes);
-    });
-  }
+  readonly settingsChanges$: Observable<{ focusTime: number; shortBreakTime: number; longBreakTime: number }> = this.settingsForm.valueChanges.pipe(
+    startWith(this.settingsForm.getRawValue()),
+    debounceTime(250),
+    map((value) => ({
+      focusTime: value.focusTime,
+      shortBreakTime: value.shortBreakTime,
+      longBreakTime: value.longBreakTime,
+    })),
+    filter(
+      (value): value is { focusTime: number; shortBreakTime: number; longBreakTime: number } =>
+        value.focusTime !== undefined &&
+        value.shortBreakTime !== undefined &&
+        value.longBreakTime !== undefined,
+    ),
+    distinctUntilChanged(
+      (prev, curr) =>
+        prev.focusTime === curr.focusTime &&
+        prev.shortBreakTime === curr.shortBreakTime &&
+        prev.longBreakTime === curr.longBreakTime,
+    ),
+    tap((changes) => {
+      console.log("Beállítás változás:", changes);
+    }),
+  );
+
+  readonly canSave$: Observable<boolean> = this.settingsForm.statusChanges.pipe(
+    startWith(this.settingsForm.status),
+    map((status) => status === "VALID"),
+    distinctUntilChanged(),
+  );
+
+  readonly saveMessage$: Observable<string> = this.saveRequested$.pipe(
+    withLatestFrom(this.settingsChanges$),
+    switchMap(([, settings]) => {
+      if (!this.settingsForm.valid) {
+        return of("Hibás adatokat adtál meg.");
+      }
+
+      return of(settings).pipe(
+        tap((currentSettings) => {
+          this.settingsService.updateSettings(
+            currentSettings.focusTime,
+            currentSettings.shortBreakTime,
+            currentSettings.longBreakTime,
+          );
+          this.timerService.resetTimer();
+        }),
+        map(() => "Beállítások elmentve."),
+        catchError(() => of("Mentési hiba történt.")),
+      );
+    }),
+    startWith(""),
+  );
 
   returnToHome(): void {
     this.router.navigateByUrl("home");
   }
 
   saveSettings() {
-    if (this.settingsForm.valid) {
-      const formValues = this.settingsForm.value;
-
-      this.settingsService.updateSettings(formValues.focusTime!, formValues.shortBreakTime!, formValues.longBreakTime!);
-
-      this.timerService.resetTimer();
-      console.log("Beállítások elmentve!");
-    }
+    this.saveRequested$.next();
   }
 }
